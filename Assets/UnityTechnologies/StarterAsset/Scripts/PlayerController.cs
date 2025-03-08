36812b90e97864a0d25584f8b5c9a5f3194c3fb6
João Pedro Bandeira Lopes <a59644@alunos.ipb.pt>
2025-03-08 21:59:17 +0000

    Acabei Player character e Game environment and physics
    Acabei Player character e Game environment and physics

--- Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    public InputAction KeyboardMoveAction;
+    Vector2 move;
+    Vector2 keyboardmove;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        KeyboardMoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
+
+        keyboardmove = KeyboardMoveAction.ReadValue<Vector2>();
+        Debug.Log(keyboardmove);
+        Vector2 keyboardposition = (Vector2)transform.position + keyboardmove * 3.0f * Time.deltaTime;
+        transform.position = keyboardposition;
+
+    }
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+        Vector2 keyboardposition = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(keyboardposition);
     }
 }
