164d2f8c357d4810c706b8e4b4383887ddbfde46
João Pedro Bandeira Lopes <a59644@alunos.ipb.pt>
2025-03-09 15:47:49 +0000

    Updates on environment and Health System
    Updates on environment and Health System

--- Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -1,42 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Movement
     public InputAction MoveAction;
-    Rigidbody2D rigidbody2d;
     public InputAction KeyboardMoveAction;
+    public float speed = 3.0f;
     Vector2 move;
     Vector2 keyboardmove;
+  
+
+    //Collisions
+    Rigidbody2D rigidbody2d;
+
+    //Health
+    public int maxHealth = 5;
+    int currentHealth;
+
+    // Variables related to temporary invincibility
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
+    public int health { get { return currentHealth; } }
+
     // Start is called before the first frame update
     void Start()
     {
+        //Activate Movement
         MoveAction.Enable();
-        rigidbody2d = GetComponent<Rigidbody2D>();
         KeyboardMoveAction.Enable();
+
+        //Activate Collisions
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        //Health
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Arrow Keys
         move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
         transform.position = position;
 
+        //WASD
         keyboardmove = KeyboardMoveAction.ReadValue<Vector2>();
         Debug.Log(keyboardmove);
-        Vector2 keyboardposition = (Vector2)transform.position + keyboardmove * 3.0f * Time.deltaTime;
+        Vector2 keyboardposition = (Vector2)transform.position + keyboardmove * speed * Time.deltaTime;
         transform.position = keyboardposition;
+
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
 
     }
     private void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        //Collisios For both the movement types
+        //Arrow Keys
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
-        Vector2 keyboardposition = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+
+        //WASD
+        Vector2 keyboardposition = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(keyboardposition);
+    }
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
